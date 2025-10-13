54c10b23abf889e9de5e4f34dfc5b6e17810053f
Renaldyarl <chagurazuran@gmail.com>
2025-10-13 13:03:29 +0700

    First Commit

---
+++ src/config/typeorm-cli.config.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { SensorReading } from 'src/sensor/sensor.entity';
+import { InitMigration1760171876792 } from 'src/migrations/1760171876792-InitMigration';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  logging: configService.get<boolean>('DB_LOGGING'),
+  entities: [SensorReading],
+  migrations: [InitMigration1760171876792],
+});
