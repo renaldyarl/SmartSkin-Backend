53526bbe6a2eeb0e23d88351b9fb439a4f3ec95e
Renaldyarl <chagurazuran@gmail.com>
2025-10-13 14:33:43 +0700

    Fixing table name 'sensor_readings'

--- src/migrations/1760171876792-InitMigration.ts
+++ src/migrations/1760171876792-InitMigration.ts
@@ -3,19 +3,19 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class InitMigration1760171876792 implements MigrationInterface {
     
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // Check if table exists dulu
         const tableExists = await queryRunner.query(`
             SELECT EXISTS (
                 SELECT FROM information_schema.tables 
                 WHERE table_schema = 'public' 
-                AND table_name = 'sensor_reading'
+                AND table_name = 'sensor_readings'
             );
-        `);
+        `)
 
         if (!tableExists[0].exists) {
             await queryRunner.query(`
-                CREATE TABLE sensor_reading (
+                CREATE TABLE sensor_readings (
                     id SERIAL PRIMARY KEY,
+                    sensor_id INT NOT NULL,
                     value FLOAT NOT NULL,
                     created_at TIMESTAMP DEFAULT NOW()
                 );
@@ -24,6 +24,6 @@ export class InitMigration1760171876792 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE IF EXISTS sensor_reading;`);
+        await queryRunner.query(`DROP TABLE IF EXISTS sensor_readings;`);
     }
 }
