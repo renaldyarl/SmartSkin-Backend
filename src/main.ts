a32777d7ef80e9c5d0383efdb19a189a4c368d61
Renaldyarl <chagurazuran@gmail.com>
2025-11-06 00:02:06 +0700

    Enabling Cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  app.setGlobalPrefix('api');
-  
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // sesuaikan dengan port React lo
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
