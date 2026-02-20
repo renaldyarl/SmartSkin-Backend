420c14bd8a26a1c3c5f404dea15b49fbcd13b80b
Renaldyarl <chagurazuran@gmail.com>
2026-02-20 16:11:44 +0700

    Updating Cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://ss.stas-rg.com/',
     Credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
