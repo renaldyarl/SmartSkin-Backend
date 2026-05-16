34cc6ee5371cc7592ce2031ac9a11cee272cd909
Renaldy Ariel <chagurazuran@gmail.com>
2026-05-16 11:59:31 +0700

    Fix: Endpoint LoRa Post

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,21 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 import { Location } from './location/location.entity';
 import { SensorType } from './sensor/sensor-type.entity';
 import { Sensor } from './sensor/sensor.entity';
 import { SensorReading } from './sensor-reading/sensor-reading.entity';
+import { Mannequin } from './mannequin/mannequin.entity';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 6789,
-  username: 'postgres',
-  password: '123',
-  database: 'hardware',
-  entities: [Location, SensorType, Sensor, SensorReading],
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'postgres',
+  entities: [Location, SensorType, Sensor, SensorReading, Mannequin],
   migrations: ['src/migrations/*.ts'],
   subscribers: [],
   synchronize: false,
